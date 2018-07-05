1e06c2cf9f4ce6cc39c576e1917a8ad8a0d4cfae
mznh <hiromtym@gmail.com>
2018-07-06 05:57:41 +0900

    不要なファイルを削除

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source "http://rubygems.org"
+gem 'rubygems'
+gem 'optparse'
+gem 'rmagick'
+gem 'curses'
--- test.rb
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env ruby
-
-require 'mini_magick'
-include MiniMagick
-
-img = Image.open(ARGV[0])
-img.resize "5x5"
-p img.get_pixels
